065e74994a80143606b96d014516c104648ebd07
Tomislav <tomislav.jakopec@gmail.com>
2025-02-05 23:18:14 -0500

    Add more styling. Add text and visual feedback on result of rounds and game.

--- script.js
+++ script.js
@@ -2,124 +2,131 @@ let possibleChoices = ["rock", "paper", "scissors"];
 let pointsComputer = 0;
 let pointsPlayer = 0;
 
-const scoreTracker = document.querySelector('.scoreTracker');
-const scoreTrackerPlayer = document.querySelector('.scorePlayer');
-const scoreTrackerComputer = document.querySelector('.scoreComp');
-
-
-
-
-
+const scoreTracker = document.getElementById("scoreTracker");
+const scoreTrackerPlayer = document.getElementById("playerScore");
+const scoreTrackerComputer = document.getElementById("computerScore");
+const selectionPlayerDiv = document.getElementById("selectionPlayer");
+const selectionComputerDiv = document.getElementById("selectionComputer");
+const resultDiv = document.getElementById("result");
 
 //Generates a random int number between 0 and 3 (excl.) and looks up the index in the array of choices
-function getComputerChoice(){
-    let choiceIndex = Math.floor(Math.random() * 3);
-    return possibleChoices[choiceIndex];
+function getComputerChoice() {
+  let choiceIndex = Math.floor(Math.random() * 3);
+  return possibleChoices[choiceIndex];
 }
 
-function addPoint(winner){
-    if (winner == "Computer") {
-        pointsComputer++;
+function addPoint(winner) {
+  if (winner == "Computer") {
+    selectionComputerDiv.style.outline = "2px solid darkslategrey";
+    selectionComputerDiv.style.backgroundColor = "red";
+    resultDiv.textContent = "Computer wins round!";
+    pointsComputer++;
+  } else if (winner == "Player") {
+    selectionPlayerDiv.style.outline = "2px solid darkslategrey";
+    selectionPlayerDiv.style.backgroundColor = "greenyellow";
+    resultDiv.textContent = "Player wins round!";
+    pointsPlayer++;
+  }
+  updateScore();
+  if (pointsComputer == 5 || pointsPlayer == 5) {
+    announceWinner();
+    removeButtons();
+  }
+}
+
+function clearOutlines() {
+  selectionPlayerDiv.style.outline = "none";
+  selectionComputerDiv.style.outline = "none";
+  selectionComputerDiv.style.backgroundColor = "";
+  selectionPlayerDiv.style.backgroundColor = "";
+}
+
+function announceWinner() {
+  let winnerMessage =
+    pointsComputer == 5 ? "Computer wins the game!" : "Player wins the game!";
+
+  resultDiv.textContent = winnerMessage;
+
+  resultDiv.classList.add("celebrate");
+}
+
+function playRound(playerSelection) {
+  clearOutlines();
+  let comp = getComputerChoice().toLowerCase();
+  updateSelections(playerSelection, comp);
+
+  if (playerSelection == comp) {
+    resultDiv.textContent = "It's a tie!";
+    return;
+  }
+  switch (comp) {
+    case "rock": {
+      if (playerSelection == "paper") {
+        addPoint("Player");
+        break;
+      } else if (playerSelection == "scissors") {
+        addPoint("Computer");
+        break;
+      }
     }
-    else if (winner == "Player"){
-        pointsPlayer++;
+    case "paper": {
+      if (playerSelection == "scissors") {
+        addPoint("Player");
+        break;
+      } else if (playerSelection == "rock") {
+        addPoint("Computer");
+        break;
+      }
     }
-    updateScore();
-    if (pointsComputer == 5 || pointsPlayer == 5){
-        announceWinner();
-        removeButtons();
+    case "scissors": {
+      if (playerSelection == "rock") {
+        addPoint("Player");
+        break;
+      } else if (playerSelection == "paper") {
+        addPoint("Computer");
+        break;
+      }
     }
+  }
 }
 
-function announceWinner(){
-    let winnerMessage = pointsComputer == 5 ? "Computer wins!" : "Player wins!";
-    
-    let announcementOfWinner = document.createElement('h2');
-    announcementOfWinner.textContent = winnerMessage;
-    scoreTracker.appendChild(announcementOfWinner);
+function updateScore() {
+  scoreTrackerPlayer.textContent = pointsPlayer;
+  scoreTrackerComputer.textContent = pointsComputer;
 }
 
-function playRound(player){
-    let comp = getComputerChoice().toLowerCase();
-    console.log(`Computer: ${comp} || Player: ${player}`);
-    
-    if (player == comp){
-        console.log("It's a tie!");
-        return;
-        }
-    switch (comp) {
-        case "rock":{
-            if (player == "paper"){
-                addPoint("Player");
-                break;
-            }
-            else if (player == "scissors"){
-                addPoint("Computer");
-                break;
-            }
-        
-        }
-        case "paper":
-            {
-            if (player == "scissors"){
-                addPoint("Player");
-                break;
-            }
-            else if (player == "rock"){
-                addPoint("Computer");
-                break;
-            }
-        }
-        case "scissors":{
-            if (player == "rock"){
-                addPoint("Player");
-                break;
-            }
-            else if (player == "paper"){
-                addPoint("Computer");
-                break;
-            }
-        }
-    }
-    
+function removeButtons() {
+  let buttons = document.getElementById("buttons");
+
+  while (buttons.firstChild) {
+    buttons.removeChild(buttons.firstChild);
+  }
+
+  let restartButton = document.createElement("button");
+  restartButton.className = "playButton";
+  restartButton.innerText = "Play again";
+  restartButton.addEventListener("click", function () {
+    window.location.reload();
+  });
+  buttons.appendChild(restartButton);
 }
 
-function updateScore(){
-    scoreTrackerPlayer.textContent = pointsPlayer;
-    scoreTrackerComputer.textContent = pointsComputer;
+function updateSelections(playerSelection, computerSelection) {
+  selectionPlayerDiv.style.backgroundImage = `url(./assets/${playerSelection}.svg)`;
+  selectionComputerDiv.style.backgroundImage = `url(./assets/${computerSelection}.svg)`;
 }
 
-function removeButtons(){
-    let buttons = document.getElementById("buttons");
-    
-    while (buttons.firstChild){
-        buttons.removeChild(buttons.firstChild);
-    }
-
-    let restartButton = document.createElement('button')
-    restartButton.className = 'playButtons';
-    restartButton.innerText = "Play again";
-    restartButton.addEventListener('click', function(){
-        window.location.reload();
-    });
-    buttons.appendChild(restartButton);
-    
-}
-
-const buttonRockPlayer = document.querySelector('#rockButtonPlayer');
-buttonRockPlayer.addEventListener('click', function(e){
-    playRound('rock');    
+const buttonRockPlayer = document.querySelector("#rockButtonPlayer");
+buttonRockPlayer.addEventListener("click", function (e) {
+  playRound("rock");
 });
 
-const buttonScissorsPlayer = document.querySelector('#scissorsButtonPlayer');
-buttonScissorsPlayer.addEventListener('click', function(e){
-    playRound('scissors');
+const buttonScissorsPlayer = document.querySelector("#scissorsButtonPlayer");
+buttonScissorsPlayer.addEventListener("click", function (e) {
+  playRound("scissors");
 });
 
-const buttonPaperPlayer = document.querySelector('#paperButtonPlayer');
-buttonPaperPlayer.addEventListener('click', function(e){
-    playRound('paper');
+const buttonPaperPlayer = document.querySelector("#paperButtonPlayer");
+buttonPaperPlayer.addEventListener("click", function (e) {
+  playRound("paper");
 });
-
-
-
